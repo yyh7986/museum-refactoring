94dc4afb6aef3dac3aba02ab8c4efd480e6ca343
yyh7986 <yyh7986@gmail.com>
2024-06-25 17:59:30 +0900

    feat: ArtworkDao mybatis 추가

---
+++ src/main/java/com/team4/artgallery/dao/IArtworkDao.java
@@ -0,0 +1,18 @@
+package com.team4.artgallery.dao;
+
+import com.team4.artgallery.vo.ArtworkVO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IArtworkDao {
+
+    List<ArtworkVO> getAllArtwork();
+
+    ArtworkVO getArtwork(int aseq);
+
+    List<ArtworkVO> getAllArtworkByCategory(String category);
+
+    List<ArtworkVO> getArtworkBySearch(String searchWord);
+}
