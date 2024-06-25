67bcdc1f6781b310d745ad2b53d378723e1ad23b
yyh7986 <yyh7986@gmail.com>
2024-06-25 18:02:02 +0900

    refactor(MemberVO): 로그인 및 회원가입 정보 validation을 위한 롬복 수정

--- src/main/java/com/team4/artgallery/vo/MemberVO.java
+++ src/main/java/com/team4/artgallery/vo/MemberVO.java
@@ -1,19 +1,25 @@
 package com.team4.artgallery.vo;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.sql.Date;
 
-@Getter
-@Setter
+@Data
 public class MemberVO {
 
+    @NotEmpty(message = "아이디를 입력하세요")
     private String id;
+    @NotEmpty(message = "이름을 입력하세요")
     private String name;
+    @NotEmpty(message = "비밀번호를 입력하세요")
     private String pwd;
+    @NotEmpty(message = "이메일을 입력하세요")
     private String email;
     private Date indate;
+    @NotEmpty(message = "전화번호를 입력하세요")
     private String phone;
     private String adminyn;
 
