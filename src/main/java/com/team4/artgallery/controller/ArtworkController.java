91b71d30ac00a325590efc6b77ed6913157b5179
yyh7986 <yyh7986@gmail.com>
2024-06-25 18:02:44 +0900

    feat: Artwork, Member Controller 구현 진행중

--- src/main/java/com/team4/artgallery/controller/ArtworkController.java
+++ src/main/java/com/team4/artgallery/controller/ArtworkController.java
@@ -1,20 +1,51 @@
 package com.team4.artgallery.controller;
 
+import com.team4.artgallery.service.ArtworkService;
+import com.team4.artgallery.util.ArtworkCategory;
+import com.team4.artgallery.vo.ArtworkVO;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/artwork")
 public class ArtworkController {
 
-    @GetMapping("/")
-    public String root() {
-        return "artwork/artworkList";
+    @Autowired
+    ArtworkService as;
+
+    @GetMapping({"","/"})
+    public ModelAndView root(HttpServletRequest request,
+                             @RequestParam(value = "category", required = false) String category,
+                             @RequestParam(value = "searchWord", required = false) String searchWord) {
+        ModelAndView mav = new ModelAndView("artwork/artworkList");
+
+        mav.addObject("category", category);
+
+        List<ArtworkVO> aList = null;
+        if(searchWord != null && !searchWord.isEmpty()) {
+            aList = as.getArtworkBySearch(searchWord);
+            mav.addObject("searchWord", searchWord);
+        }else if(category == null || category.isEmpty() || category.equals("전체")){
+            aList = as.getAllArtwork();
+        }else{
+            aList = as.getAllArtworkByCategory(category);
+        }
+        mav.addObject("artworkList", aList);
+        return mav;
     }
 
     @GetMapping("/view")
-    public String view() {
+    public String view(@RequestParam("aseq") int aseq, Model model) {
+        model.addAttribute("artwork", as.getArtwork(aseq));
         return "artwork/artworkView";
     }
 
--- src/main/java/com/team4/artgallery/controller/MemberController.java
+++ src/main/java/com/team4/artgallery/controller/MemberController.java
@@ -1,17 +1,41 @@
 package com.team4.artgallery.controller;
 
+import com.team4.artgallery.service.MemberService;
+import com.team4.artgallery.vo.MemberVO;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController {
 
+    @Autowired
+    MemberService ms;
+
     @GetMapping("/login")
     public String login() {
         return "member/loginForm";
     }
+
+    @PostMapping("/login")
+    public String login(HttpServletRequest request,
+                        @ModelAttribute MemberVO membervo, BindingResult result, Model model) {
+        if(result.getFieldError("id") != null){
+            model.addAttribute("message", result.getFieldError("id").getDefaultMessage());
+        }else if(result.getFieldError("pwd") != null){
+            model.addAttribute("message", result.getFieldError("pwd").getDefaultMessage());
+        }else{
+            MemberVO mvo = ms.getMember(membervo.getId());
+        }
+    }
+
 
     @GetMapping("/join")
     public String register() {
